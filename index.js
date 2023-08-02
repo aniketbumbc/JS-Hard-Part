8f8718fec774c61c4b51c55c045649c38f6c64bc
aniket <aniket.umbc@gmail.com>
2023-08-02 11:51:38 +0530

    callback with numbers

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function displayCallback() {
   return 400 * 200;
 }
 
-displayName('Aniket', displayCallback);
+//displayName('Aniket', displayCallback);
 
 function calculate(number, callBackFun) {
   return callBackFun(number) * number;
@@ -17,4 +17,19 @@ function multiplyBy2(number) {
   return number * 2;
 }
 
-console.log(calculate(100, multiplyBy2));
+//console.log(calculate(100, multiplyBy2));
+
+function map(array, callback) {
+  const output = [];
+
+  for (let ele of array) {
+    output.push(callback(ele));
+  }
+  return output;
+}
+
+function addTwo(input) {
+  return input * 2;
+}
+
+console.log(map([1, 2, 3], addTwo));
