f53b2bd21aa328b2c100bf4e9ad782d87c2a3da4
aniket <aniket.umbc@gmail.com>
2023-08-03 10:21:22 +0530

    Clousure practice session

---
+++ clousure.js
@@ -0,0 +1,51 @@
+function outerFuntion() {
+  function innerFunction() {
+    console.log('Welcome to inner Function');
+  }
+
+  innerFunction();
+}
+
+outerFuntion();
+
+function createFunction() {
+  function multiplyBy2(num) {
+    return num * 2;
+  }
+
+  return multiplyBy2;
+}
+
+let generatedFunction = createFunction();
+let result = generatedFunction(100);
+// console.log(result);
+
+/**
+ *  Increment counter using closure
+ */
+
+function outer() {
+  let counter = 0;
+
+  function inner() {
+    return counter++;
+  }
+
+  return inner;
+}
+
+let resutOuter = outer();
+// resutOuter();
+// resutOuter();
+// resutOuter();
+// resutOuter();
+// resutOuter();
+
+// console.log(resutOuter());
+// console.log(resutOuter());
+// console.log(resutOuter());
+// console.log(resutOuter());
+// console.log(resutOuter());
+// console.log(resutOuter());
+// console.log(resutOuter());
+// console.log(resutOuter());
