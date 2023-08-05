69ff704aac1d48bab8ee5b63c59ad3f9ffa64a62
aniket <aniket.umbc@gmail.com>
2023-08-05 13:08:51 +0530

    async code

--- asyncode.js
+++ asyncode.js
@@ -34,4 +34,31 @@ function callBackHellCode() {
   }, 1000);
 }
 
-callBackHellCode();
+// callBackHellCode();
+
+function delayCounter(target, wait) {}
+
+// UNCOMMENT THESE TO TEST YOUR WORK!
+// const countLogger = delayCounter(3, 1000)
+// countLogger();
+// After 1 second, log 1
+// After 2 seconds, log 2
+// After 3 seconds, log 3
+
+/* CHALLENGE 8 */
+
+function delayCounter(target, wait) {
+  function inner() {
+    for (let i = 1; i <= target; i++) {
+      setTimeout(() => {
+        console.log(`After ${i} second`);
+      }, wait * i);
+    }
+  }
+
+  return inner;
+}
+
+const countLogger = delayCounter(3, 5000);
+
+countLogger();
